7b4b6b89ba9403e7867207f2e69b516b4a6ae448
Walfas <kirbehm@gmail.com>
2013-04-23 23:33:53 -0400

    fix texture atlas bleeding (hopefully)

--- js/src/texture.js
+++ js/src/texture.js
@@ -1,10 +1,11 @@
 define(["gl"], function(gl) {
 	return {
 		TextureAtlas: function(url,tileSize) {
-			this.tileSizePx = [tileSize,tileSize];
-			this.imageSizePx = [0,0];
-			this.tileSizeNormalized = [0,0];
+			this.tileSizePx = tileSize;
+			this.imageSizePx = 0;
+			this.tileSizeNormalized = 0;
 			this.tilesPerRow = 0;
+			this.paddingNormalized = 0;
 			this.texture = null;
 
 			// Load from URL
@@ -21,30 +22,25 @@ define(["gl"], function(gl) {
 				gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-				//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-				//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-				gl.generateMipmap(gl.TEXTURE_2D);
 				gl.bindTexture(gl.TEXTURE_2D, null);
 
 				this.texture = texture;
-				this.imageSizePx = [image.width, image.height];
+				this.imageSizePx = image.width; // width must equal height
 
-				this.tileSizeNormalized = [
-					this.tileSizePx[0]/this.imageSizePx[0], 
-					this.tileSizePx[1]/this.imageSizePx[1]
-				];
-				this.tilesPerRow = Math.floor(this.imageSizePx[0]/this.tileSizePx[0]);
+				this.tileSizeNormalized = this.tileSizePx/this.imageSizePx;
+				this.paddingNormalized = 0.5/this.imageSizePx;
+				this.tilesPerRow = Math.floor(this.imageSizePx/this.tileSizePx);
 			}
 
 			/** Based on tile number, get the s and t coordinate ranges of the tile.
 				returns array of format [s1,t1,s2,t2] */
 			this.getST = function(tileNum) {
 				var stRange = [
-					this.tileSizeNormalized[0] * (tileNum % this.tilesPerRow),
-					this.tileSizeNormalized[1] * Math.floor(tileNum / this.tilesPerRow),
+					this.tileSizeNormalized * (tileNum % this.tilesPerRow) + this.paddingNormalized,
+					this.tileSizeNormalized * Math.floor(tileNum / this.tilesPerRow) + this.paddingNormalized,
 				];
-				stRange[2] = stRange[0] + this.tileSizeNormalized[0];
-				stRange[3] = stRange[1] + this.tileSizeNormalized[1];
+				stRange[2] = stRange[0] + this.tileSizeNormalized - this.paddingNormalized;
+				stRange[3] = stRange[1] + this.tileSizeNormalized - this.paddingNormalized;
 				return stRange;
 			}
 
